1be2a98135676e74a37b75d8bb7ca267b29dfe10
mendy <maouizerat@gmail.com>
2021-05-18 16:41:37 +0300

    Initial Commit

---
+++ newgitProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  newgitProject
+//
+//  Created by mendy aouizerat  on 18/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
